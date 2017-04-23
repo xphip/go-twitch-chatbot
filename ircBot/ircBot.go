553307a365542ebac0bb8e0c5fa7daa27292ab05
xphip <xphipme@gmail.com>
2017-04-23 20:04:33 -0300

    Correções e adição de comentários no package

--- ircBot/ircBot.go
+++ ircBot/ircBot.go
@@ -9,15 +9,6 @@ import (
 	"net/textproto"
 )
 
-type BotInterface interface{
-	Connect(conn net.Conn, err error)
-	Send(string)
-}
-
-type CH struct {
-	PingLoop chan bool
-}
-
 type Bot struct {
 	Server string
 	Port string
@@ -31,19 +22,18 @@ type Bot struct {
 	Buffer *textproto.Reader
 }
 
-func NewBot(server string, port string, nick string, user string, channel string, pass string) *Bot {
+// Cria novo Bot
+func NewBot(server string, port string, nick string, channel string, pass string) *Bot {
 	return &Bot{
 		Server: server,
 		Port: port,
 		Nick: nick,
-		User: user,
 		Channel: channel,
 		Pass: pass,
-		// Conn: nil,
-		// Buffer: nil,
 	}
 }
 
+// Conecta o bot
 func (bot *Bot) Connect() (conn net.Conn, err error) {
 	bot.Conn, err = net.Dial("tcp", bot.Server + ":" + bot.Port)
 	if err != nil{
@@ -63,6 +53,7 @@ func (bot *Bot) Connect() (conn net.Conn, err error) {
 	return conn, err
 }
 
+// Cria o buffer
 func (bot *Bot) Maker() (err error) {
 	reader := bufio.NewReader(bot.Conn)
 	bot.Buffer = textproto.NewReader(reader)
@@ -70,22 +61,28 @@ func (bot *Bot) Maker() (err error) {
 	return err
 }
 
+// Envia comando puro para o IRC
 func (bot *Bot) Send(command string) {
 	fmt.Fprintf(bot.Conn, "%s\r\n", command)
 }
 
+// Envia uma mensagem para um determinado canal
 func (bot *Bot) Msg(channel string, msg string) {
 	fmt.Fprintf(bot.Conn, "PRIVMSG #%s :%s\r\n", channel, msg)
 }
 
+// Entra em um canal
 func (bot *Bot) Join(channel string) {
 	fmt.Fprintf(bot.Conn, "JOIN #%s\r\n", channel)
 }
 
+// Sai de um canal
 func (bot *Bot) Leave(channel string) {
 	fmt.Fprintf(bot.Conn, "PART #%s\r\n", channel)
 }
 
+// Função que dispara uma checagem pra dizer
+//	"servidor eu to vivo", a cada 2 minutos
 func (bot *Bot) PingLoop() (err error) {
 	for {
 		time.Sleep(time.Second * 120)
